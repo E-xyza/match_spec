86a7a174f0dd66328af68f0c38658488511c65b1
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2022-12-21 23:59:04 -0600

    mix information, license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule MatchSpec.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "helper module for match specs",
+      package: package(),
+      source_url: "https://github.com/ityonemo/match_spec",
+      docs: [
+        main: "MatchSpec",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -24,6 +31,15 @@ defmodule MatchSpec.MixProject do
       {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.6"},
       {:ex_doc, "~> 0.29.1"}
+    ]
+  end
+
+  defp package do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ityonemo/match_spec"}
     ]
   end
 end
